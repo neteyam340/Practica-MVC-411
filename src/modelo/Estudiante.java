cd72ada8dd5f0aa418465bbd5c81111dedb03fc0
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-04-22 19:38:03 -0500

    Agregando la clase modelo estudiante y creando la estructura MVC

---
+++ src/modelo/Estudiante.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author g.perezmoreno
+ */
+public class Estudiante {
+    private String nombre;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
