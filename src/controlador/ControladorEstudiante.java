fede8580424793392fee45fecc8e519657ea1282
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-04-22 19:49:20 -0500

    Agregando la clase controlador estudiante

---
+++ src/controlador/ControladorEstudiante.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import modelo.Estudiante;
+import vista.VistaEstudiante;
+
+/**
+ *
+ * @author g.perezmoreno
+ */
+public class ControladorEstudiante {
+    private Estudiante modelo;
+    private VistaEstudiante vista;
+
+    public ControladorEstudiante(Estudiante modelo, VistaEstudiante vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+    
+    public void setNombreEstudiante(String nombre) {
+        modelo.setNombre(nombre);
+    }
+    
+     public String getNombreEstudiante() {
+        return modelo.getNombre();
+    }
+     
+     public void setEdadEstudiante(int edad) {
+        modelo.setEdad(edad);
+    }
+    
+     public int getEdadEstudiante() {
+        return modelo.getEdad();
+    }
+    
+     public void actualizarVista() {
+         vista.mostrarDetallesEstudiante(modelo.getNombre(), modelo.getEdad());
+     }
+}
